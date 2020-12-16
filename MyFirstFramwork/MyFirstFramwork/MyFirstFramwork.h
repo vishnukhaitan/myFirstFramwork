44f7672053723dd0da45ca2150881bc9f78edc54
Vishnu Khaitan <vishnu.khaitan@daytonatec.com>
2020-12-16 11:29:25 +0530

    initial commit

---
+++ MyFirstFramwork/MyFirstFramwork/MyFirstFramwork.h
@@ -0,0 +1,19 @@
+//
+//  MyFirstFramwork.h
+//  MyFirstFramwork
+//
+//  Created by vishnu khaitan on 16/12/20.
+//  Copyright © 2020 Diasporatec LTD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyFirstFramwork.
+FOUNDATION_EXPORT double MyFirstFramworkVersionNumber;
+
+//! Project version string for MyFirstFramwork.
+FOUNDATION_EXPORT const unsigned char MyFirstFramworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFirstFramwork/PublicHeader.h>
+
+
