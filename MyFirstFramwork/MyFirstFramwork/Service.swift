74e5ece59b99d243943e75526313f1839d38a9f4
Vishnu Khaitan <vishnu.khaitan@daytonatec.com>
2020-12-16 11:37:16 +0530

    added service class

---
+++ MyFirstFramwork/MyFirstFramwork/Service.swift
@@ -0,0 +1,20 @@
+//
+//  Service.swift
+//  MyFirstFramwork
+//
+//  Created by vishnu khaitan on 16/12/20.
+//  Copyright © 2020 Diasporatec LTD. All rights reserved.
+//
+
+import Foundation
+
+open class Service {
+    
+    private init (){
+        
+    }
+    
+    public static func doSomeStuff()->String{
+        return "this is my call back"
+    }
+}
